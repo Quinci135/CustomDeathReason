488f520305a0da234558e8a2eb80f1580bf10766
Quinci135 <quinci135@gmail.com>
2020-07-12 06:16:19 -0700

    Remove excess memorystream. Terraria MessageBuffer already implements a MessageBuffer which can be used directly, which is what tshock does too

--- CustomDeathReason/CustomDeath.cs
+++ CustomDeathReason/CustomDeath.cs
@@ -46,53 +46,52 @@ namespace CustomDeath
         {
             if (args.MsgID == PacketTypes.PlayerDeathV2)
             {
-                using (MemoryStream data = new MemoryStream(args.Msg.readBuffer, args.Index, args.Length))
+
+                int player = args.Msg.readerStream.ReadByte();
+                PlayerDeathReason deathReason = PlayerDeathReason.FromReader(new BinaryReader(args.Msg.readerStream));
+                int damage = args.Msg.readerStream.ReadInt16();
+                int direction = args.Msg.readerStream.ReadByte() - 1;
+                bool pvp = ((BitsByte)args.Msg.readerStream.ReadByte())[0];
+                args.Handled = true;
+                TSPlayer.All.SendInfoMessage($"{TShock.Players[player].Name} has died");
+                string customDeathReason = "yogurt";
+                TSPlayer.All.SendInfoMessage($"damage: [c/afef2a:{damage}]");
+                TSPlayer.All.SendInfoMessage($"deathReason._sourceItemType: {deathReason._sourceItemType}");
+                TSPlayer.All.SendInfoMessage($"deathReason._sourceProjectileType: {deathReason._sourceProjectileType}");
+                TSPlayer.All.SendInfoMessage($"deathReason._sourceProjectileIndex: {deathReason._sourceProjectileIndex}");
+                TSPlayer.All.SendInfoMessage($"deathReason._sourceNPCIndex: {deathReason._sourceNPCIndex}");
+                TSPlayer.All.SendInfoMessage($"deathReason._sourcePlayerIndex: {deathReason._sourcePlayerIndex}");
+                TSPlayer.All.SendInfoMessage($"deathReason._sourceItemPrefix: {deathReason._sourceItemPrefix}");
+                TSPlayer.All.SendInfoMessage($"deathReason._sourceOtherIndex: {deathReason._sourceOtherIndex}");
+                TSPlayer.All.SendInfoMessage($"pvp: {pvp}");
+                Projectile proj = new Projectile();
+                proj.SetDefaults(deathReason._sourceProjectileType);
+
+                if (pvp)
                 {
-                    int player = data.ReadByte();
-                    PlayerDeathReason deathReason = PlayerDeathReason.FromReader(new BinaryReader(data));
-                    int damage = data.ReadInt16();
-                    int direction = data.ReadByte() - 1;
-                    bool pvp = ((BitsByte)data.ReadByte())[0];
-                    args.Handled = true;
-                    TSPlayer.All.SendInfoMessage($"{TShock.Players[player].Name} has died");
-                    string customDeathReason = "yogurt";
-                    TSPlayer.All.SendInfoMessage($"damage: [c/afef2a:{damage}]");
-                    TSPlayer.All.SendInfoMessage($"deathReason._sourceItemType: {deathReason._sourceItemType}");
-                    TSPlayer.All.SendInfoMessage($"deathReason._sourceProjectileType: {deathReason._sourceProjectileType}");
-                    TSPlayer.All.SendInfoMessage($"deathReason._sourceProjectileIndex: {deathReason._sourceProjectileIndex}");
-                    TSPlayer.All.SendInfoMessage($"deathReason._sourceNPCIndex: {deathReason._sourceNPCIndex}");
-                    TSPlayer.All.SendInfoMessage($"deathReason._sourcePlayerIndex: {deathReason._sourcePlayerIndex}");
-                    TSPlayer.All.SendInfoMessage($"deathReason._sourceItemPrefix: {deathReason._sourceItemPrefix}");
-                    TSPlayer.All.SendInfoMessage($"deathReason._sourceOtherIndex: {deathReason._sourceOtherIndex}");
-                    TSPlayer.All.SendInfoMessage($"pvp: {pvp}");
-                    Projectile proj = new Projectile();
-                    proj.SetDefaults(deathReason._sourceProjectileType);
-
-                    if (pvp)
-                    {
-                        customDeathReason = (deathReason._sourcePlayerIndex == 0) ? $"[c/00ccff:{TShock.Players[player].Name}] was dealt [c/afef2a:[c/afef2a:{damage}] mortal damage by their own [i/p{deathReason._sourceItemPrefix}:{deathReason._sourceItemType}]" : $"[c/00ccff:{TShock.Players[player].Name}] was dealt [c/afef2a:{damage}] fatal damage by {TShock.Players[deathReason._sourcePlayerIndex].Name}'s [i/p{deathReason._sourceItemPrefix}:{deathReason._sourceItemType}]";
-                    }
-                    else if (deathReason._sourceNPCIndex != -1)
-                    {
-                        customDeathReason = $"[c/00ccff:{TShock.Players[player].Name}] was dealt [c/afef2a:{damage}] mortal damage by [c/00ff00:{Terraria.Main.npc[deathReason._sourceNPCIndex].TypeName}]";
-                    }
-                    else if (deathReason._sourceOtherIndex == -1 && proj.hostile)
-                    {
-                        customDeathReason = $"[c/00ccff:{TShock.Players[player].Name}] was dealt [c/afef2a:{damage}] mortal damage by [c/00ff00:{proj.Name}]";
-                    }
-                    else if (deathReason._sourceOtherIndex == 4)
-                    {
-                        string evil = WorldGen.crimson ? "Crimson" : "Demon";
-                        customDeathReason = $"[c/00ccff:{TShock.Players[player].Name}] was driven insane trying to smash a [c/00ff00:{evil} altar]";
-                    }
-
-                    if (customDeathReason != "yogurt")
-                    {
-                        deathReason = PlayerDeathReason.ByCustomReason(customDeathReason);
-                    }
-
-                    Terraria.Main.player[player].KillMe(deathReason, damage, direction, pvp);
+                    customDeathReason = (deathReason._sourcePlayerIndex == 0) ? $"[c/00ccff:{TShock.Players[player].Name}] was dealt [c/afef2a:[c/afef2a:{damage}] mortal damage by their own [i/p{deathReason._sourceItemPrefix}:{deathReason._sourceItemType}]" : $"[c/00ccff:{TShock.Players[player].Name}] was dealt [c/afef2a:{damage}] fatal damage by {TShock.Players[deathReason._sourcePlayerIndex].Name}'s [i/p{deathReason._sourceItemPrefix}:{deathReason._sourceItemType}]";
                 }
+                else if (deathReason._sourceNPCIndex != -1)
+                {
+                    customDeathReason = $"[c/00ccff:{TShock.Players[player].Name}] was dealt [c/afef2a:{damage}] mortal damage by [c/00ff00:{Terraria.Main.npc[deathReason._sourceNPCIndex].TypeName}]";
+                }
+                else if (deathReason._sourceOtherIndex == -1 && proj.hostile)
+                {
+                    customDeathReason = $"[c/00ccff:{TShock.Players[player].Name}] was dealt [c/afef2a:{damage}] mortal damage by [c/00ff00:{proj.Name}]";
+                }
+                else if (deathReason._sourceOtherIndex == 4)
+                {
+                    string evil = WorldGen.crimson ? "Crimson" : "Demon";
+                    customDeathReason = $"[c/00ccff:{TShock.Players[player].Name}] was driven insane trying to smash a [c/00ff00:{evil} altar]";
+                }
+
+                if (customDeathReason != "yogurt")
+                {
+                    deathReason = PlayerDeathReason.ByCustomReason(customDeathReason);
+                }
+
+                Terraria.Main.player[player].KillMe(deathReason, damage, direction, pvp);
+            
             }
         }
     }
